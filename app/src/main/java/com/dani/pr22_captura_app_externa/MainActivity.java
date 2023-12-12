ab75d0db5726ba487163819230f641499dd450a4
VDani3 <villacarmona.dani@gmail.com>
2023-12-12 18:01:30 +0100

    Version 3

--- app/src/main/java/com/dani/pr22_captura_app_externa/MainActivity.java
+++ app/src/main/java/com/dani/pr22_captura_app_externa/MainActivity.java
@@ -5,26 +5,36 @@ import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
+    String currentPhotoPath = "";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Button takeF = findViewById(R.id.foto);
         Button makef = findViewById(R.id.makef);
+        Button fSize = findViewById(R.id.fullsize);
         ImageView f = findViewById(R.id.imageView);
 
         ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
@@ -36,7 +46,6 @@ public class MainActivity extends AppCompatActivity {
                             // There are no request codes
                             Intent data = result.getData();
                             Uri uri = data.getData();
-
                             f.setImageURI(uri);
                         }
                     }
@@ -78,5 +87,63 @@ public class MainActivity extends AppCompatActivity {
                 someActivityResultLauncherV2.launch(intent);
             }
         });
+
+        fSize.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dispatchTakePictureIntent();
+            }
+        });
     }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+
+        // Create the File where the photo should go
+        File photoFile = null;
+        try {
+            photoFile = createImageFile();
+        } catch (IOException ex) {
+            // Error occurred while creating the File
+        }
+        // Continue only if the File was successfully created
+        if (photoFile != null) {
+            Uri photoURI = FileProvider.getUriForFile(this,
+                    "com.dani.pr22_captura_app_externa.fileprovider",
+                    photoFile);
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+            someActivityResultLauncherV3.launch(takePictureIntent);
+        }
+    }
+
+    ActivityResultLauncher<Intent> someActivityResultLauncherV3 = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        ImageView imageView = findViewById(R.id.imageView);
+                        imageView.setImageURI(Uri.parse(currentPhotoPath));
+                    }
+                }
+            }
+    );
+
 }
